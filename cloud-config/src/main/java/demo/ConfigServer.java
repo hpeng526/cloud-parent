7545e2c7778e1c11770c966761fa404139f58b57
hpeng526 <hpeng526@gmail.com>
2017-04-14 09:36:55 +0800

    init

---
+++ cloud-config/src/main/java/demo/ConfigServer.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by peng_huang on 2017/4/13.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
---
+++ cloud-eureka/src/main/java/demo/EurekaServer.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by peng_huang on 2017/4/13.
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
